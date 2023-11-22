668a02de459967a066babffb2097b4bcf53301b6
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-11-22 20:15:12 -0300

    clion filho da puta vai na força do ódio porra do caralho hein

---
+++ common/communication/protocol.cpp
@@ -0,0 +1,80 @@
+#include "protocol.hpp"
+
+#include <unistd.h>
+
+#include <climits>
+#include <fstream>
+
+bool dropbox::HeaderExchange::Send() {
+    return write(socket_, &this->command_, sizeof(this->command_)) == sizeof(this->command_);
+}
+
+bool dropbox::HeaderExchange::Receive() {
+    return read(socket_, &this->command_, sizeof(this->command_)) == sizeof(this->command_);
+}
+
+bool dropbox::FileExchange::Send() {
+    if (!SendPath()) {
+        return false;
+    }
+
+    std::basic_ifstream<uint8_t> file(path_, std::ios::in | std::ios::binary);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    while (!file.eof()) {
+        const auto    kBytesRead = static_cast<size_t>(file.read(buffer_->data(), kPacketSize).gcount());
+        const ssize_t kBytesSent = write(socket_, buffer_->data(), kBytesRead);
+
+        if (kBytesSent < kBytesRead) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool dropbox::FileExchange::Receive() {
+    if (!ReceivePath()) {
+        return false;
+    }
+
+    if (!std::filesystem::is_regular_file(path_)) {
+        std::filesystem::remove_all(path_);
+    }
+
+    std::basic_ofstream<uint8_t> file(path_, std::ios::out | std::ios::binary | std::ios::trunc);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    do {
+        const auto kBytesReceived = read(socket_, buffer_->data(), kPacketSize);
+
+        if (kBytesReceived == -1)
+        {
+            return false;
+        }
+
+        file.write(buffer_->data(), kBytesReceived);
+    } while (!file.eof());
+}
+
+bool dropbox::EntryExchange::SendPath() {
+    const auto kConvertedPath = path_.generic_u8string();
+    return write(socket_, kConvertedPath.data(), kConvertedPath.size()) == kConvertedPath.size();
+}
+
+bool dropbox::EntryExchange::ReceivePath() {
+    static std::u8string received_path(PATH_MAX, u8'\0');
+
+    if (read(socket_, received_path.data(), PATH_MAX) == -1)
+    {
+        return false;
+    }
+
+    path_ = received_path;
+    return true;
+}
---
+++ common/communication/protocol.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <sys/types.h>
+
+#include <array>
+#include <cstdint>
+#include <filesystem>
+
+namespace dropbox {
+enum class Command { USERNAME, UPLOAD, DELETE, GET_SYNC_DIR, EXIT, LIST_SERVER };
+
+class Exchange {
+   public:
+    virtual ~Exchange()                  = default;
+    [[nodiscard]] virtual bool Send()    = 0;
+    [[nodiscard]] virtual bool Receive() = 0;
+};
+
+class HeaderExchange : public Exchange {
+   public:
+    inline constexpr HeaderExchange(int socket) noexcept : socket_(socket){};
+
+    inline constexpr void SetCommand(Command new_command) noexcept { command_ = new_command; }
+
+    [[nodiscard]] bool Send() override;
+    [[nodiscard]] bool Receive() override;
+
+   private:
+    int     socket_;
+    Command command_;
+};
+
+class EntryExchange : public Exchange {
+   public:
+    explicit EntryExchange(int socket) : socket_(socket) {}
+
+    [[nodiscard]] bool SendPath();
+    [[nodiscard]] bool ReceivePath();
+
+    inline void SetPath(std::filesystem::path&& path) { path_ = std::move(path); }
+
+   protected:
+    int                   socket_;
+    std::filesystem::path path_;
+};
+
+class FileExchange : public EntryExchange {
+   public:
+    inline FileExchange(int socket) : EntryExchange(socket){};
+
+    [[nodiscard]] bool Send() override;
+    [[nodiscard]] bool Receive() override;
+
+   private:
+    static constexpr size_t kPacketSize = 64U * 1024U;
+
+    int                                               socket_;
+    std::unique_ptr<std::array<uint8_t, kPacketSize>> buffer_;
+};
+
+class DirectoryExchange : public EntryExchange {
+    /// @todo This
+};
+}
