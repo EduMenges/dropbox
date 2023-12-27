4975d2047b29d96ad820f500e7050457d44094e8
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-27 15:36:09 -0300

    Added signal handling (can't debug it and I want to die)

--- server/main.cpp
+++ server/main.cpp
@@ -1,24 +1,35 @@
 #include <charconv>
 #include <cstring>
+#include <csignal>
+
 #include <iostream>
 
 #include "server.hpp"
 
 namespace dropbox {
-enum ArgV : size_t { EXECUTION_PATH [[maybe_unused]] = 0U, PORT, TOTAL };
+enum ArgV : size_t { kExecutionPath [[maybe_unused]] = 0U, kPort, kTotal };
 }
 
 using enum dropbox::ArgV;
 using dropbox::ArgV;
 
+static void SignalHandler(int signal) {
+    std::cout << "Shuting down\n";
+}
+
 int main(int argc, char* argv[]) {
-    if (static_cast<ArgV>(argc) != TOTAL) {
+    if (static_cast<ArgV>(argc) != kTotal) {
         std::cerr << "Usage: <port>\n";
         return EXIT_FAILURE;
     }
 
+    if (signal(SIGINT, SignalHandler) == SIG_ERR) {
+        std::cerr << "Failure on setting sigal handler\n";
+        return EXIT_FAILURE;
+    }
+
     in_port_t port = 0;
-    auto [ptr, ec] = std::from_chars(argv[PORT], argv[PORT] + strlen(argv[PORT]), port);
+    auto [ptr, ec] = std::from_chars(argv[kPort], argv[kPort] + strlen(argv[kPort]), port);
 
     if (ec == std::errc()) {
         try {
